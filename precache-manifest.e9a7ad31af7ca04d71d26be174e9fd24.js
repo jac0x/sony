d0482b1815b1acde18b9b379a99158f71efc9f10
shark <taidashark@outlook.com>
2018-12-04 13:55:01 +0800

    initial commit

---
+++ precache-manifest.e9a7ad31af7ca04d71d26be174e9fd24.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bf2f8b017f8b9037e5735f1c18d0054",
+    "url": "/static/media/clap.9bf2f8b0.svg"
+  },
+  {
+    "revision": "cc7e4e0237851a4388ef",
+    "url": "/static/css/main.906670b4.chunk.css"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "01eb3acc8b20284a9cd9",
+    "url": "/static/js/1.01eb3acc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6e6386e5eae11510637f2d2115e4cbb",
+    "url": "/static/media/logo-sony.f6e6386e.svg"
+  },
+  {
+    "revision": "102603df3bbf0276ebef41abd9f27f08",
+    "url": "/static/media/slider-image1-hoteltransylvanie3.102603df.png"
+  },
+  {
+    "revision": "2a8737e1608b16a65291bcbc787ecf5c",
+    "url": "/static/media/iconfinder_facebook_2119360.2a8737e1.svg"
+  },
+  {
+    "revision": "0ba3a74fcc6e266d169b2301db090405",
+    "url": "/static/media/iconfinder_twitter_2119367.0ba3a74f.svg"
+  },
+  {
+    "revision": "6a231bdf4941fe58d3a273ce4796775b",
+    "url": "/static/media/iconfinder_instagram_2119361.6a231bdf.svg"
+  },
+  {
+    "revision": "24ef645299dd30ee8d11e1265ab712d2",
+    "url": "/static/media/Fond-2-sony.24ef6452.svg"
+  },
+  {
+    "revision": "9a178706d53dfd8b020bdf121cba5294",
+    "url": "/static/media/carte.9a178706.svg"
+  },
+  {
+    "revision": "498a377018493e3f5ed038e61deec3f9",
+    "url": "/static/media/chapeau.498a3770.svg"
+  },
+  {
+    "revision": "bd4914baf8b849adbdb4a0d92d996fcb",
+    "url": "/static/media/pack.bd4914ba.svg"
+  },
+  {
+    "revision": "cc7e4e0237851a4388ef",
+    "url": "/static/js/main.cc7e4e02.chunk.js"
+  },
+  {
+    "revision": "836bd987611c23a175b8f2a04a878b3f",
+    "url": "/static/media/smartphone.836bd987.svg"
+  },
+  {
+    "revision": "36c28d8720f06ca80134311b37742b7e",
+    "url": "/static/media/mcdopetit2.36c28d87.jpg"
+  },
+  {
+    "revision": "2f743d1ae39b67c579709fe044d2bea3",
+    "url": "/static/media/mcdoscratch.2f743d1a.jpg"
+  },
+  {
+    "revision": "e588de039702dccef0a77a876b4db069",
+    "url": "/static/media/cinema-compressor.e588de03.png"
+  },
+  {
+    "revision": "e48f5d9d91098dc2782c216400f5f483",
+    "url": "/static/media/Fond-2-sony-V2.e48f5d9d.svg"
+  },
+  {
+    "revision": "072f133d9cb5314941a65355d3c76de5",
+    "url": "/static/media/logoDarty.072f133d.jpg"
+  },
+  {
+    "revision": "ee5e4c23b4758d2ab51bda1764d9639d",
+    "url": "/static/media/3boites.ee5e4c23.png"
+  },
+  {
+    "revision": "77180b3ae7bbb24186203de331e0eeff",
+    "url": "/static/media/tablette-2.77180b3a.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "01eb3acc8b20284a9cd9",
+    "url": "/static/css/1.b4c2e1ef.chunk.css"
+  },
+  {
+    "revision": "4a72cbe1c0905088725f0bec33f095c9",
+    "url": "/index.html"
+  }
+];
